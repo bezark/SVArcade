9e5add28a024f70c85eef5ba0d745f745f1c4273
bezark <jdb459@nyu.edu>
2025-03-11 16:53:42 -0400

    Update grid_loader.gd

--- Menus/grid_loader.gd
+++ Menus/grid_loader.gd
@@ -4,7 +4,7 @@ extends GridContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#grab_focus()
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	for game in class_projects.projects:
 		var new_ui = game_ui.instantiate()
